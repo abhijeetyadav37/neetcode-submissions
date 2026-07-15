bdb1b85c332fb3398c821c320b857c9571a310ac
AbhiJeet Yadav <abhijeetyadav1508@gmail.com>
2026-07-15 22:54:07 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.java
@@ -0,0 +1,21 @@
+// using equls method with string builder 
+// O(n)
+
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        StringBuilder cleaned = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+
+            if (Character.isLetterOrDigit(c)) {
+                cleaned.append(Character.toLowerCase(c));
+            }
+        }
+
+        String original = cleaned.toString();
+        String reversed = cleaned.reverse().toString();
+
+        return original.equals(reversed);
+    }
+}
